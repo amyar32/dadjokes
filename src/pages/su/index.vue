43733165854e01df74db8f710499750df0456a76
A. Y. Firdaus <amyar32@gmail.com>
2021-10-15 21:45:48 +0700

    add new sunda

---
+++ src/pages/su/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <jokes-card joke="Acan aya jokes na, sabar"></jokes-card>
+</template>
+
+<script setup>
+import JokesCard from "../../components/ui/JokesCard.vue";
+</script>
--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import index from "./pages/index.vue";
 import en from "./pages/en/index.vue";
 import id from "./pages/id/index.vue";
+import su from "./pages/su/index.vue";
 
 const routes = [
   { path: "/", component: index },
   { path: "/en", component: en },
   { path: "/id", component: id },
+  { path: "/su", component: su },
 ];
 
 const router = createRouter({
