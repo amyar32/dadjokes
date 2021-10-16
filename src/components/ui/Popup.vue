9c9424d69058a50495518319e4f96c74971aca90
A. Y. Firdaus <amyar32@gmail.com>
2021-10-16 14:27:19 +0700

    new comp

---
+++ src/components/ui/Popup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="fixed bottom-10 left-10 py-3 px-6 rounded-lg text-white shadow-lg"
+    :class="{
+      'bg-green-600': props.color === 'green',
+      'bg-red-600': props.color === 'red',
+      'bg-yellow-600': props.color === 'yellow',
+    }"
+  >
+    {{ props.message }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  message: String,
+  color: String,
+});
+</script>
