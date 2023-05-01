41640497ca040de334f8e30d0b4d7416dd812669
Amin Yarits Firdaus <amyar32@gmail.com>
2023-05-01 21:22:35 +0700

    feat(base): add linter

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'airbnb-base',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
